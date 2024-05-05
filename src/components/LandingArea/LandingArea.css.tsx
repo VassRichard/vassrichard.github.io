1543c56a0b87735db5e826388a4edd3a2aef46fc
Richard-Daniel Vass <vassrichard09@yahoo.com>
2024-05-05 18:22:03 +0300

    Enhanced UX for mobile devices

--- src/components/LandingArea/LandingArea.css.tsx
+++ src/components/LandingArea/LandingArea.css.tsx
@@ -13,14 +13,19 @@ export const LandingAreaWrapper = styled.div<{
 `;
 
 export const DescriptionWrapper = styled.div`
-    width: 35%;
     top: 17.5%;
-    left: 5%;
     position: absolute;
     color: White;
 
+    ${minWidthQuery(Breakpoints.nano)} {
+        width: 90%;
+        left: 5%;
+        right: 5%;
+    }
+
     ${minWidthQuery(Breakpoints.medium)} {
         width: 30%;
+        left: 5%;
     }
 
     ${minWidthQuery(Breakpoints.mediumExtra)} {
@@ -33,15 +38,21 @@ export const PersonWrapper = styled.div`
     height: 85vh;
     position: relative;
     top: 15%;
-    margin: 0 0 0 5.5rem;
     opacity: 0.75;
     background: url(${PersonImage}) center center;
     background-size: contain;
     background-repeat: no-repeat;
 
+    ${minWidthQuery(Breakpoints.nano)} {
+        height: 40vh;
+        top: 60.5%;
+        margin: 0;
+    }
+
     ${minWidthQuery(Breakpoints.medium)} {
         height: 80vh;
         top: 20%;
+        margin: 0 0 0 5.5rem;
     }
 
     ${minWidthQuery(Breakpoints.large)} {
@@ -54,6 +65,11 @@ export const RoleWrapper = styled.div`
     width: 50%;
     font-family: 'Aspekta', sans-serif;
     letter-spacing: 0.15rem;
+
+    ${minWidthQuery(Breakpoints.nano)} {
+        width: 100%;
+        font-size: 2rem;
+    }
 
     ${minWidthQuery(Breakpoints.medium)} {
         width: 88.75%;
@@ -80,6 +96,14 @@ export const Separator = styled.div`
     background: White;
     margin: 2rem 0 2rem 0;
     opacity: 0.3;
+
+    ${minWidthQuery(Breakpoints.nano)} {
+        width: 100%;
+    }
+
+    ${minWidthQuery(Breakpoints.medium)} {
+        width: 50%;
+    }
 `;
 
 export const SeparatorSpace = styled.div<{ marginValue: number }>`
@@ -91,8 +115,13 @@ export const RoleDescription = styled.div`
     font-size: 0.9rem;
     font-weight: 50;
 
+    ${minWidthQuery(Breakpoints.nano)} {
+        text-align: center;
+    }
+
     ${minWidthQuery(Breakpoints.medium)} {
         font-size: 0.75rem;
+        text-align: start;
     }
 
     ${minWidthQuery(Breakpoints.large)} {
